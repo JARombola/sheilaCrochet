7e20bfc33e2ce34f530ab54a424fcdc5ca0e3f98
Julian Rombola <jrombola@varegos.local>
2020-12-05 10:54:24 -0300

    Actualización Precios Antes Fin de Año

--- src/repo-productos.service.ts
+++ src/repo-productos.service.ts
@@ -26,6 +26,8 @@ export class RepoProductosService {
     this.productosList.push(this.chaquetaGladysVideo());
     this.productosList.push(this.claseMateCorazon());
     this.productosList.push(this.cursoChalecoSheila());
+    this.productosList.push(this.sweaterDora());
+    this.productosList.push(this.remeraGraciela());
     // Gratis
     this.productosList.push(this.mandalas());
     this.productosList.push(this.musculosaFlor());
@@ -50,7 +52,7 @@ export class RepoProductosService {
     producto.descripcionLarga = 'En el curso te enseño cómo adaptar el Cárdigan a todos tus gusto: si lo querés más largo, más corto, si querés tan sólo la espalda más larga. Además te explico dos diseños de mangas! También te enseño cómo adaptarlo a  cualquier hilado.';
     producto.detalles = 'Incluye acceso a clases de consulta vía mail.\n' +
     'Las clases ya están grabadas: podes verlas CUANDO quieras y LAS VECES que quieras.';
-    producto.precio = 860;
+    producto.precio = 917;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-78d992ba-2ee5-4de0-a681-528ea6e4399e';
     producto.imagenPortada = 'cardiganAnna/cardiganAnnaMin.jpg';
     producto.imagenesUrl = ['cardiganAnna/cardiganAnna1.jpg', 'cardiganAnna/cardiganAnna2.jpg', 'cardiganAnna/cardiganAnna3.jpg'];
@@ -64,7 +66,7 @@ export class RepoProductosService {
     producto.descripcionCorta = 'Dos increíbles prendas que no pueden faltar entre las de primavera/verano.';
     producto.descripcionLarga = 'La Chaqueta Gladys es ideal para los cambios de estación, va a ser tu aliada en la primavera. Y la Musculosa Haydeé es SÚPER fresca para el verano. La mejor combinación para pasar estas estaciones!';
     producto.detalles = 'Guía explicativa PDF chaqueta gladys + Guía explicativa PDF musculosa haydeé.\n (No incluye videos).';
-    producto.precio = 606;
+    producto.precio = 827;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-d7698859-6fc0-47db-99b2-888c23cf15d6';
     producto.imagenPortada = 'primaveraVerano/adm-primaveraVeranoMin.jpg';
     producto.imagenesUrl = ['primaveraVerano/adm-primaveraVerano.jpg'];
@@ -79,7 +81,7 @@ export class RepoProductosService {
     producto.descripcionLarga = 'Curso para aquellas que nunca tejieron y quieren aprender. Se realizan 5 proyectos en los que vas a aprender los puntos básicos y formas básicas. Aprenderás “la base del crochet” y estarás lista para seguir aprendiendo más de esta bella técnica.';
     producto.detalles = 'Incluye acceso a clases de consulta vía mail.\nLas clases ya están grabadas: podés verlas CUÁNDO quieras y LAS VECES que quieras.\n'
     + 'Además... ¡INCLUYE PROYECTO DE REGALO!';
-    producto.precio = 800;
+    producto.precio = 1025;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-40bd7cb0-21b2-4353-9d04-c8dc6f9f04cc';
     producto.imagenPortada = 'cursoPrincipiantes/adm-cursoPrincipiantesMin.png';
     producto.imagenesUrl = ['cursoPrincipiantes/adm-cursoPrincipiantes1.png', 'cursoPrincipiantes/adm-cursoPrincipiantes2.jpg', 'cursoPrincipiantes/adm-cursoPrincipiantes3.jpg',
@@ -93,7 +95,7 @@ export class RepoProductosService {
     producto.descripcionCorta = 'Sweater súper práctico y cómodo que vas a amar desde el primer instante.';
     producto.descripcionLarga = 'Te enseño a hacer el sweater a tu gusto! Vas a aprender a adaptarlo a cualquier talle y preferencia: más al cuerpo, más ancho, mangas anchas, mangas más angostas. Te enseño el paso a paso pero también te doy el espacio para que dejes volar tu creatividad! Además vas a poder adaptarlo a cualquier hilado.';
     producto.detalles = 'Incluye acceso a clases de consulta vía mail.\n Las clases ya están grabadas: podés verlas CUÁNDO quieras y LAS VECES que quieras.';
-    producto.precio = 800;
+    producto.precio = 1025;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-9f248b70-59e0-406c-a1a3-8046b9fdb236';
     producto.imagenPortada = 'sweaterMariela/adm-sweaterMarielaMin.png';
     producto.imagenesUrl = ['sweaterMariela/adm-sweaterMariela1.png', 'sweaterMariela/adm-sweaterMariela2.png', 'sweaterMariela/adm-sweaterMariela3.png'];
@@ -107,7 +109,7 @@ export class RepoProductosService {
     producto.descripcionCorta = 'Prenda fresca que vas a amar. Además es SÚPER FÁCIL de hacer. Ideal para tejer como primer prenda.';
     producto.descripcionLarga = 'Esta musculosa es ideal para vos si nunca tejiste ninguna prenda. Es súper práctica y fácil de hacer. Además es muy cómoda y te enseño a adaptarla a cualquier talle e hilado. \nTambién incluye tabla de talles para que, si no querés sacar cuentas, vayas siguiendo mis instrucciones para poder tejerla.';
     producto.detalles = 'PDF Explicativo: Paso a paso para adaptar la prenda a cualquier talle e hilado.\nExplicación para realizarla en 5 talles.\n(No contiene videos).';
-    producto.precio = 356;
+    producto.precio = 647;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-67af5d07-b46a-4284-af5f-41756e001947';
     producto.imagenPortada = 'musculosaHaydee/adm-musculosaHaydeeMin.jpg';
     producto.imagenesUrl = ['musculosaHaydee/adm-musculosaHaydee1.png', 'musculosaHaydee/adm-musculosaHaydee2.png', 'musculosaHaydee/adm-musculosaHaydee3.png'];
@@ -121,7 +123,7 @@ export class RepoProductosService {
     producto.descripcionCorta = 'Prenda fresca que vas a amar. Además es SÚPER FÁCIL de hacer. Ideal para tejer como primer prenda.';
     producto.descripcionLarga = 'Esta musculosa es ideal para vos si nunca tejiste ninguna prenda. Es súper práctica y fácil de hacer. Además es muy cómoda y te enseño a adaptarla a cualquier talle e hilado. \nTambién incluye tabla de talles para que, si no querés sacar cuentas, vayas siguiendo mis instrucciones para poder tejerla.';
     producto.detalles = 'PDF Explicativo: Paso a paso para adaptar la prenda a cualquier talle e hilado.\nExplicación para realizarla en 5 talles.\nINCLUYE VIDEOS EXPLICATIVOS.';
-    producto.precio = 500;
+    producto.precio = 863;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-eb1d52b3-15fc-41fd-8cf5-057d9c4bbd9c';
     producto.imagenPortada = 'musculosaHaydee/adm-musculosaHaydeeMin.jpg';
     producto.imagenesUrl = ['musculosaHaydee/adm-musculosaHaydee1.png', 'musculosaHaydee/adm-musculosaHaydee2.png', 'musculosaHaydee/adm-musculosaHaydee3.png'];
@@ -135,21 +137,21 @@ export class RepoProductosService {
     producto.descripcionCorta = 'Prenda ideal para media estación. Además cuenta con una gran ventaja al tejerla... ¡NO LLEVA COSTURAS! ';
     producto.descripcionLarga = 'La Chaqueta Gladys es una prenda que va CON TODO. La podés usar para completar tanto un outfit informal como formal. Es ideal para media estación y es aún más ideal porque… NO LLEVA COSTURA. Se teje en una sola pieza y por eso es súper fácil y práctica.';
     producto.detalles = 'PDF EXPLICATIVO: Paso a paso para adaptar la prenda a cualquier talle e hilado (NO incluye videos).';
-    producto.precio = 356;
+    producto.precio = 647;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-74a1a351-2371-472a-a3c7-56c308c57e7c';
     producto.imagenPortada = 'chaquetaGladys/adm-chaquetaGladysMin.jpg';
     producto.imagenesUrl = ['chaquetaGladys/adm-chaquetaGladys1.png', 'chaquetaGladys/adm-chaquetaGladys2.png', 'chaquetaGladys/adm-chaquetaGladys3.png', 'chaquetaGladys/adm-chaquetaGladys4.png'];
     producto.conocimientos = 'Saber contar puntos, vueltas, realizar puntos básicos (vareta, media vareta, medio punto, punto deslizado) y saber leer gráficos.';
     return producto;
   }
-  
+
   private chaquetaGladysVideo(): Producto {
     const producto = new Producto();
     producto.nombre = 'Chaqueta Gladys (PDF + Videos)';
     producto.descripcionCorta = 'Prenda ideal para media estación. Además cuenta con una gran ventaja al tejerla... ¡NO LLEVA COSTURAS! ';
     producto.descripcionLarga = 'La Chaqueta Gladys es una prenda que va CON TODO. La podés usar para completar tanto un outfit informal como formal. Es ideal para media estación y es aún más ideal porque… NO LLEVA COSTURA. Se teje en una sola pieza y por eso es súper fácil y práctica.';
     producto.detalles = 'PDF EXPLICATIVO: Paso a paso para adaptar la prenda a cualquier talle e hilado.\nINCLUYE VIDEOS.';
-    producto.precio = 500;
+    producto.precio = 827;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-b907761e-f776-45fe-a36b-3148d9e19a40';
     producto.imagenPortada = 'chaquetaGladys/adm-chaquetaGladysMin.jpg';
     producto.imagenesUrl = ['chaquetaGladys/adm-chaquetaGladys1.png', 'chaquetaGladys/adm-chaquetaGladys2.png', 'chaquetaGladys/adm-chaquetaGladys3.png', 'chaquetaGladys/adm-chaquetaGladys4.png'];
@@ -163,7 +165,7 @@ export class RepoProductosService {
     producto.descripcionCorta = 'Acceso a una clase dónde vas a poder aprender a hacer este Mate Corazón para que el momento de tomar mate se convierta en algo mucho más especial.';
     producto.descripcionLarga = 'Paso a paso en donde vamos a realizar la decoración del Mate Corazón. El momento del mate será mucho más especial y bonito. Y porqué no, crear el tuyo propio, con tus colores y tu estilo. Así, cuando nos volvamos a encontrar cada uno con su mate, vos vas a disfrutar del tuyo personalizado.';
     producto.detalles = 'Videos ya grabados: Podés ver la clase CUANDO QUIERAS Y LAS VECES QUE QUIERAS.\nAcceso a consultas vía mail.';
-    producto.precio = 290;
+    producto.precio = 324;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-39ae4649-c639-4b69-a0a7-7d12df023105';
     producto.imagenPortada = 'mateCorazon/adm-mateCorazonMin.jpg';
     producto.imagenesUrl = ['mateCorazon/adm-mateCorazon1.jpg', 'mateCorazon/adm-mateCorazon2.jpg', 'mateCorazon/adm-mateCorazon3.jpg'];
@@ -178,7 +180,7 @@ export class RepoProductosService {
     producto.descripcionLarga = 'El Chaleco Sheila es ideal para el invierno y media estación. Los chalecos son muy prácticos porque nos mantienen abrigadas pero de una manera cómoda. Sobre todo para aquellas que no les gusta estar con mucha ropa. Lo podes usar de manera informal con alguna remera de manga largo o de manera formal con una camisa… ¿Cómo lo usarías';
     producto.detalles = 'Paso a paso para adaptar la prenda a cualquier talle e hilado. Las clases son videos ya grabados, que vas a poder ver CUANDO QUIERAS Y LAS VECES QUE QUIERAS.\n.'
     + 'En este curso te enseño a hacer aumentos y disminuciones. De esta manera el chaleco se adapta perfecto a cada parte de tú cuerpo para que puedas sentirte cómoda por completo!';
-    producto.precio = 800;
+    producto.precio = 917;
     producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-178c07aa-ac49-4bc0-bc86-8cfdbbd05f6b';
     producto.imagenPortada = 'chalecoSheila/adm-chalecoSheilaMin.jpg';
     producto.imagenesUrl = ['chalecoSheila/adm-chalecoSheila1.jpg', 'chalecoSheila/adm-chalecoSheila2.png', 'chalecoSheila/adm-chalecoSheila3.jpg', 'chalecoSheila/adm-chalecoSheila4.jpg', 'chalecoSheila/adm-chalecoSheila5.jpg'];
@@ -186,7 +188,42 @@ export class RepoProductosService {
     return producto;
   }
 
-  // GRATIS
+  private sweaterDora(): Producto {
+    const producto = new Producto();
+    producto.nombre = 'Sweater Dora';
+    producto.descripcionCorta = '¿Querés tejer TU PRIMERA PRENDA? ¿Querés que sea súper fácil y rápido de hacer? Esta es LA MEJOR OPCIÓN.';
+    producto.descripcionLarga = 'El Sweater... es simple, sencillo de hacer y súper fácil. Su simpleza y frescura hace que te enamores completamente de él! ¿Lo mejor? LO VAS A TEJER RAPIDÍSIMO Y SIN COSTURAS. ¿Estás lista para que tejamos juntas?';
+    producto.detalles = 'Ideal para PRINCIPIANTES del crochet.\n'
+    + 'VIDEOS YA FILMADOS: Podés ver las clases CUANDO quieras y LAS VECES que quieras. Se adapta perfecto a tu rutina! Vos decidís tu ritmo de tejido, y yo TE ACOMPAÑO en el proceso.\n'
+    + 'No tiene fecha límite!\n'
+    + 'Clases de consulta vía mail: A medida que vas tejiendo, podés mandarme tus consultas y despejar dudas.';
+    producto.precio = 1025;
+    producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-adeba107-2825-49cd-8a3b-069b5eaec402';
+    producto.imagenPortada = 'sweaterDora/adm-sweaterDoraMin.jpeg';
+    producto.imagenesUrl = ['sweaterDora/adm-sweaterDora1.png', 'sweaterDora/adm-sweaterDora2.jpeg', 'sweaterDora/adm-sweaterDora3.jpeg'];
+    producto.conocimientos = 'Conocimientos básicos de crochet y del Punto Vareta';
+    return producto;
+  }
+
+  private remeraGraciela(): Producto {
+    const producto = new Producto();
+    producto.nombre = 'Remera Graciela';
+    producto.descripcionCorta = '¿Estás preparada para tejer en modo primavera Crochetera? En este Curso te traigo una bella y dulce opción!'
+    producto.descripcionLarga = 'Es una remera muy sencilla de realizar, ideal si querés tejer tu primera prenda. Es fresca y MUY cómoda, porque te enseño a adaptarla a tu gusto y talle. Además contas con mi apoyo constante a medida que la vas haciendo. Podés hacerme las preguntas que necesites!';
+    producto.detalles = 'GUIA PDF. Vas a encontrar el paso a paso escrito + gráficos.\n'
+    + 'VIDEOS YA FILMADOS: Podés ver las clases CUANDO quieras y LAS VECES que quieras. Se adapta perfecto a tu rutina!\n'
+    + 'VOS DECIDÍS TU RITMO de tejido, y yo TE ACOMPAÑO en el proceso. No tiene fecha límite.\n'
+    + 'CLASES DE CONSULTA VÍA MAIL DE LUNES A VIERNES: A medida que vas tejiendo, podés mandarme mails con tus consultas y despejar dudas.';
+    producto.precio = 917;
+    producto.urlMP = 'https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=259278692-9994464c-ad89-4205-9a6f-923bee9a1b01';
+    producto.imagenPortada = 'remeraGraciela/adm-remeraGracielaMin.png';
+    producto.imagenesUrl = ['remeraGraciela/adm-remeraGraciela1.jpg', 'remeraGraciela/adm-remeraGraciela2.png', 'remeraGraciela/adm-remeraGraciela3.png', 'remeraGraciela/adm-remeraGraciela4.jpg', 'remeraGraciela/adm-remeraGraciela5.jpg'];
+    producto.conocimientos = 'Deberás contar con conocimientos mínimos de crochet (sobre todo punto vareta), pero no hace falta ser una experta! Ya que está remera ES SÚPER FÁCIL';
+    return producto;
+  }
+  // --------------------------------------------------------------------------------------
+  // --------------------------------------- GRATIS ---------------------------------------
+  // --------------------------------------------------------------------------------------
   private musculosaFlor(): Producto {
     const producto = new Producto();
     producto.nombre = 'Musculosa Flor';
